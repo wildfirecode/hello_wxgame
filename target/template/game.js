7ff3e0b70aedf444cd2cedf033f1459ece40caf0
honingwon <honingwon@gmail.com>
2017-12-30 20:22:22 +0800

    update

---
+++ target/template/game.js
@@ -0,0 +1,16 @@
+require('./weapp-adapter.js')
+require('./manifest.js')
+
+egret.runEgret({
+    renderMode: "webgl", audioType: 0, calculateCanvasScaleFactor: function (context) {
+        var backingStore = context.backingStorePixelRatio ||
+            context.webkitBackingStorePixelRatio ||
+            context.mozBackingStorePixelRatio ||
+            context.msBackingStorePixelRatio ||
+            context.oBackingStorePixelRatio ||
+            context.backingStorePixelRatio || 1;
+        return (window.devicePixelRatio || 1) / backingStore;
+    }
+});
+
+// require("egret.min.js")
