ec53ef2533130df04cfa2e591a75d94e09b5e60f
honingwon <honingwon@gmail.com>
2017-12-30 20:23:57 +0800

    update

---
+++ egret-project/libs/exml.e.d.ts
@@ -0,0 +1,52 @@
+declare module skins{
+	class ButtonSkin{
+	}
+}
+declare module skins{
+	class CheckBoxSkin{
+	}
+}
+declare module skins{
+	class HScrollBarSkin{
+	}
+}
+declare module skins{
+	class HSliderSkin{
+	}
+}
+declare module skins{
+	class ItemRendererSkin{
+	}
+}
+declare module skins{
+	class PanelSkin{
+	}
+}
+declare module skins{
+	class ProgressBarSkin{
+	}
+}
+declare module skins{
+	class RadioButtonSkin{
+	}
+}
+declare module skins{
+	class ScrollerSkin{
+	}
+}
+declare module skins{
+	class TextInputSkin{
+	}
+}
+declare module skins{
+	class ToggleSwitchSkin{
+	}
+}
+declare module skins{
+	class VScrollBarSkin{
+	}
+}
+declare module skins{
+	class VSliderSkin{
+	}
+}
